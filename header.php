dcde651efa272b8f0108896338268adbc874a10d
Michael Hemingway <mikemingway@gmail.com>
2017-08-31 11:50:14 -0400

    Improved SCSS structure and @media mixin

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 	<link href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 	<?php wp_head(); ?>
-
+	<link href="https://fonts.googleapis.com/css?family=Oswald:600" rel="stylesheet">
 	<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
